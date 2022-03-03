49cac2146da37c70b8d011b001259748742a1f59
Bob VanLonkhuyzen <hey.bob.oh@gmail.com>
2022-03-03 12:48:01 -0500

    Get task id from github pr context

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const fetch = require('node-fetch');
+const core = require('@actions/core')
+const github = require('@actions/github')
+const fetch = require('node-fetch')
 
+const prBody = github.context.payload.pull_request?.body
+console.log(prBody)
+if (!prBody) {
+  core.setFailed('Could not retrieve PR body')
+}
+const taskID = prBody.slice(0, prBody.indexOf('\n'))
 const endpoint = "/projects/api/v3/tasks/"
-let url = "https://" + core.getInput('domain') + endpoint + core.getInput('task_id') + '/complete.json';
+let url = "https://" + core.getInput('domain') + endpoint + core.getInput('task_id') + '/complete.json'
 
 // Sends a PUT request to Teamwork to set the task as complete
 const putOpts = {
@@ -15,11 +21,8 @@ const putOpts = {
   },
   redirect: 'follow',
   body: 'false'
-};
+}
 
 fetch(url, putOpts)
   .then(response => {if (response.status != 204) {throw "Server returned " + response.status}})
-  .catch(err => core.setFailed(err.message));
-
-const context = github.context;
-console.log(context.payload.pull_request?.body);
+  .catch(err => core.setFailed(err.message))
