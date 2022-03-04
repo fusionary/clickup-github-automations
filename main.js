c55712d1b1b2e43241d168d2b82047009fc15c11
Bob VanLonkhuyzen <hey.bob.oh@gmail.com>
2022-03-03 19:48:10 -0500

    Set logging line back to pr body

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const github = require('@actions/github')
 const fetch = require('node-fetch')
 
 const prBody = github.context.payload.pull_request?.body
-console.log(github.context)
+console.log(prBody)
 if (!prBody) {
   core.setFailed('Could not retrieve PR body')
   return
