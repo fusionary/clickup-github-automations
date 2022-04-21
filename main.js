1644906ddea7cf052204333a779b20b7b9b35058
Bob VanLonkhuyzen <hey.bob.oh@gmail.com>
2022-04-21 15:45:40 -0400

    Go one nested level deeper to get project id

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ async function moveCard(task, columnName) {
   // Unfortunately, the v3 api doesn't include the project id in the task data like v1 does. Therefor for now, we use the v1 endpoint until this is resolved
   const taskV1Endpoint = '/tasks/'
   let taskV1Url = 'https://' + core.getInput('domain') + taskV1Endpoint + taskID + '.json'
-  const projectID = await getRequest(taskV1Url)['project-id']
+  const projectID = await getRequest(taskV1Url)['todo-item']['project-id']
 
   let columnID = 0
   const boardEndpoint = '/projects/'
